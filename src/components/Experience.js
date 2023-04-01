bcd81f9ced97b8954fb89cf88c57558db67d35bc
Goncervan <gonfedecer@gmail.com>
2023-04-01 17:46:18 -0300

    finish nav v1

---
+++ src/components/Experience.js
@@ -0,0 +1,6 @@
+import React from 'react'
+export const Experience = () => {
+  return (
+    <div id='Experience' className='w-full h-screen seccion pt-28'>Experience</div>
+  )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,6 @@
+import React from 'react'
+export const Projects = () => {
+  return (
+    <div id='Projects' className='w-full h-screen seccion pt-28'>Projects</div>
+  )
+}
