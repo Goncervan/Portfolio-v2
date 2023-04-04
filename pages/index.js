d90edea8f2e84a6bf22e2d21aebabc6d6744b851
Goncervan <gonfedecer@gmail.com>
2023-04-03 21:31:26 -0300

    aboutMe

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,12 @@ import { Experience } from "../src/components/Experience";
 import { Skills } from "../src/components/Skills";
 import { Education } from "../src/components/Education";
 import { Contact } from "../src/components/Contact";
+import { Presentation } from "../src/components/Presentation";
 import { NavMobile } from "../src/components/Navs/NavMobile";
 export default function Home() {
   return (
     <div className="relative">
+      <Presentation />
       <AboutMe />
       <NavWeb />
       <NavMobile />
