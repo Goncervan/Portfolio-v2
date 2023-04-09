18547496ae3d6b11d3b9c4bcc23af59505375eab
Goncervan <gonfedecer@gmail.com>
2023-04-09 00:11:32 -0300

    presentation mobile

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -4,18 +4,33 @@ import Image from "next/image";
 import FotoPerfil from "../../public/Imagenes/Foto.jpg";
 export const Presentation = () => {
   return (
-    <div className="w-full h-[90vh] bg-customGray flex items-start justify-center flex-col px-28">
+    <div className="w-full h-[90vh] bg-customGray flex items-start justify-center flex-col px-10 sm:px-28">
       <div className="w-full absolute top-0 left-0 border-b-2 border-gray-600 h-14 flex justify-between items-center pr-20 pl-10">
-        <p className="text-white font-semibold text-lg">Gonzalo Cervan</p>
-        <p className="text-white font-semibold text-lg">gonfedecer@gmail.com</p>
+        <p className="text-white font-semibold text-base sm:text-lg">
+          Gonzalo Cervan
+        </p>
+        <p className="text-white font-semibold sm:inline hidden sm:text-lg">
+          gonfedecer@gmail.com
+        </p>
       </div>
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{duration:2}} className="w-1/2 h-full flex flex-col items-start pt-20 justify-center">
-        <h1 className="text-[5rem] text-white font-bold">FRONT</h1>
-        <h1 className="text-[5rem] italic text-white font-bold">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 0.6 }}
+        className="w-full sm:w-1/2 h-full flex flex-col items-start pt-5 sm:pt-20 justify-center"
+      >
+        <h1 className="text-[3rem] sm:text-[5rem] text-white font-bold">
+          FRONT
+        </h1>
+        <h1 className="text-[3rem] sm:text-[5rem] italic text-white font-bold">
           END <span className="font-normal">{"</>"}</span>
         </h1>
-        <h1 className="text-[5rem] text-white font-bold">DEVELOPER</h1>
-        <button className="text-xl text-customBlue border-2 border-customBlue rounded-full px-4 py-1 focus:outline-none ml-28 mt-10">Contacto</button>
+        <h1 className="text-[3rem] sm:text-[5rem] text-white font-bold">
+          DEVELOPER
+        </h1>
+        <button className="text-xl text-customBlue border-2 border-customBlue rounded-full px-4 py-1 focus:outline-none mx-auto sm:ml-28 mt-10">
+          Contacto
+        </button>
       </motion.div>
       <div className="w-1/2"></div>
     </div>
--- src/components/Projects/Card.js
+++ src/components/Projects/Card.js
@@ -5,9 +5,9 @@ export const Card = ({ title, description, img, link, index }) => {
     <div
       className={`w-full border-b border-white flex ${
         index % 2 === 0 ? "flex-row" : "flex-row-reverse"
-      } justify-between border-opacity-20 px-40 py-8`}
+      } justify-between border-opacity-20 px-40 py-12`}
     >
-      <div className="w-2/5 flex flex-col items-center justify-center h-[250px]">
+      <div className="w-2/5 flex flex-col items-center justify-center h-[275px]">
         <h3 className="text-2xl text-white flex-1">{title}</h3>
         <p className="text-customBlue flex-1 text-center">{description}</p>
         <a
@@ -18,7 +18,7 @@ export const Card = ({ title, description, img, link, index }) => {
           Visitar
         </a>
       </div>
-      <div className="w-2/5 flex flex-col items-start justify-start h-[250px]">
+      <div className="w-2/5 flex flex-col items-start justify-start h-[275px]">
         {img !== undefined && (
           <div className="border-2 rounded-xl bg-customBlue border-customBlue">
             <Image src={img} alt={title} className="rounded-xl" />
