5c6cb9db4e3940efa7719d3e5bd298d06cb2c2eb
Goncervan <gonfedecer@gmail.com>
2023-04-09 00:26:37 -0300

    aboutme y skills mobile

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,35 +5,35 @@ export const AboutMe = () => {
   return (
     <div
       id="hero"
-      className="w-full min-h-[90vh] bg-customGray flex flex-col items-center justify-center overflow-hidden pb-10"
+      className="w-full min-h-[90vh] bg-customGray flex flex-col items-center justify-center overflow-hidden sm:pb-10 px-10 sm:px-0"
     >
       <motion.div
         initial={{ y: 500, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 1, type: "spring" }}
         viewport={{ once: true }}
-        className="w-4/5 flex items-start justify-between gap-10"
+        className="w-full sm:w-4/5 flex flex-col sm:flex-row items-start justify-between gap-10"
       >
-        <section className="w-2/5 flex flex-col items-start justify-start">
-          <h1 className="text-[35px] text-white font-bold uppercase mb-10 relative">
-            <span className="text-customBlue absolute -left-5">. </span>
+        <section className="w-full sm:w-2/5 flex flex-col items-start justify-start">
+          <h1 className="text-3xl sm:text-[35px] text-white font-bold uppercase mb-4 sm:mb-10 relative">
+            <span className="text-customBlue absolute -left-3 sm:-left-5">. </span>
             Acerca de mí
           </h1>
           <ul className="flex flex-col gap-4 w-full">
             <li>
-              <p className="text-white text-xl">
+              <p className="text-white text-base sm:text-xl">
                 Mi nombre es Gonzalo Cervan, tengo 21 años y soy de Argentina.
               </p>
             </li>
             <li>
-              <p className="text-white text-xl">
+              <p className="text-white text-base sm:text-xl">
                 Mis intereses incluyen unirme a un equipo emocionante de gente
                 apasionada, que me ayuden a crecer de manera profesional y
                 personal y participar de proyectos desafiantes e innovadores.
               </p>
             </li>
             <li>
-              <p className="text-white text-xl">
+              <p className="text-white text-base sm:text-xl">
                 Aparte de programar me gusta hacer deporte, pasar el tiempo con
                 mi familia y amigos y los videojuegos.
               </p>
@@ -43,40 +43,40 @@ export const AboutMe = () => {
             Ver mi CV
           </button>
         </section>
-        <section className="w-2/5 h-full flex flex-col items-center justify-start">
-          <h1 className="text-[35px] text-white font-bold uppercase relative mb-10">
-            <span className="text-customBlue absolute -left-5">. </span>
+        <section className="w-full sm:w-2/5 h-full flex flex-col items-center justify-start">
+          <h1 className="text-3xl sm:text-[35px] text-white font-bold uppercase relative mb-10">
+            <span className="text-customBlue absolute -left-3 sm:-left-5">. </span>
             Habilidades
           </h1>
-          <ul className="w-full flex flex-wrap items-start justify-center gap-5 px-4">
-            <li className="text-white text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
+          <ul className="w-full flex flex-wrap items-start justify-center gap-5 sm:px-4">
+            <li className="text-white text-base sm:text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
               Ingles Avanzado
             </li>
-            <li className="text-white text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
+            <li className="text-white text-base sm:text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
               Trabajo en equipo
             </li>
-            <li className="text-white text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
+            <li className="text-white text-base sm:text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
               Creatividad
             </li>
-            <li className="text-white text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
+            <li className="text-white text-base sm:text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
               Curiosidad
             </li>
-            <li className="text-white text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
+            <li className="text-white text-base sm:text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
               Atención al detalle
             </li>
-            <li className="text-white text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
+            <li className="text-white text-base sm:text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
               Adaptabilidad
             </li>
-            <li className="text-white text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
+            <li className="text-white text-base sm:text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
               Buena comunicación
             </li>
-            <li className="text-white text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
+            <li className="text-white text-base sm:text-xl border border-white rounded-full px-4 py-1 cursor-default hover:bg-white hover:text-black transition-all">
               Pensamiento crítico
             </li>
           </ul>
         </section>
       </motion.div>
-      <Skills></Skills>
+      <Skills/>
     </div>
   );
 };
